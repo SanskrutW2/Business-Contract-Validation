49feb22c3ba1adde8c722531586cb183600c9614
SanskrutW2 <156744165+SanskrutW2@users.noreply.github.com>
2024-07-14 21:46:07 +0530

    Add files via upload

---
+++ L3_Streamlit/main.py
@@ -0,0 +1,66 @@
+import sys
+import os
+
+from ContractValidator import ContractValidator
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../L1_individual_components/')))
+
+def validatContract(inputPdf, templatePdf,agreeType, clasue , heading):
+
+    try:
+
+        # creating model 
+        model = ContractValidator(inputPdf, templatePdf, agreeType , clasue , heading)
+
+        # parsing pdfs 
+        print("Parsing pdf :\n")
+        inputText = model.parseInputPdf()
+        templateText = model.parseTemplatePdf()
+
+
+        # ner for input pdf 
+        print("Performing NER :\n")
+        inputPdfNer = model.performNer(inputText)
+        print("Printing ner response :\n\n")
+        print(inputPdfNer)
+        hashable_ner_dict = tuple(sorted(inputPdfNer.items()))
+        
+        # classify text 
+        print("Performing classification :\n")
+        inputClassifiedText = model.classifyInputText()
+        templateClassifiedText = model.classifyTemplateText()
+
+        print("inputclassifiedtext:\n", inputClassifiedText,'\n\n\n')
+        print("tempclassifiedtext:\n", templateClassifiedText,'\n\n\n')
+
+        print("Finding deviations :\n")
+        # compare classified text 
+        dev_dic , dev_words = model.compareText(inputClassifiedText, templateClassifiedText)
+
+        # highligh pdf 
+        print("Performing highlighting :\n")
+        highlitedPdf = model.highlightPdf(dev_words)
+
+        print("Printing summary :\n")
+        # summary of pdf 
+        summary = model.getSummary(inputPdfNer, inputText)
+
+        # returning output
+        print("Returning to node server :\n")
+        return {
+            "compare_dic": dev_dic,
+            "highlitedPdf": highlitedPdf,
+            "summary": summary,
+            "ner_dict": inputPdfNer
+        }
+
+
+    except Exception as err:
+        print("error occured in main function")
+        raise Exception(f"error: {err}")
+
+
+
+if __name__ == "__main__":
+    validatContract()
+
